00c2bce162dec72ee44eb0d07aaa8ea13ed4383a
andrea yamileth <andreachaquinga2005@gmail.com>
2024-12-01 15:16:34 -0500

    Automovil

---
+++ src/main/java/com/mycompany/automovil/Automovil.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.automovil;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Automovil {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
